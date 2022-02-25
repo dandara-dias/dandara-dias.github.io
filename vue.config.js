6e31340a248324b4fcf5368ee9816b6e3becde92
dandara-dias <dandaradias.contato@gmail.com>
2022-02-25 20:48:11 -0300

    Subindo o repo

--- vue.config.js
+++ vue.config.js
@@ -3,5 +3,5 @@ const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
-  publicPath: process.env.NODE_ENV === 'production' ? '/REPO_NAME/' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/' : '/',
 });
