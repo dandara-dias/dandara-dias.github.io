22a391e23974cf00d2e6dc4501aa179a1eb15ee9
dandara-dias <dandaradias.contato@gmail.com>
2022-02-25 20:51:29 -0300

    URL

--- vue.config.js
+++ vue.config.js
@@ -3,5 +3,5 @@ const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
-  publicPath: process.env.NODE_ENV === 'production' ? '/home' : '/',
+  publicPath: process.env.NODE_ENV === '/',
 });
