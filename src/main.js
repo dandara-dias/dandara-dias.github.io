d9d225e2ff076b55e2d6022c76f7f7562ff2ecc9
dandara-dias <dandaradias.contato@gmail.com>
2022-03-09 19:11:47 -0300

    About - background 1

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue';
+import Particles from 'particles.vue3';
 import App from './App.vue';
 import router from './router';
 
-createApp(App).use(router).mount('#app');
+createApp(App).use(router).use(Particles).mount('#app');
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,81 @@
+// src: https://dev.to/matteobruni/how-to-create-beautiful-particles-effect-in-vue-js-2-x-and-3-x-27d8
+
 <template>
-  <h1>Teste</h1>
+  <div id="app">
+    <h1>Teste</h1>
+    <Particles
+                id="tsparticles"
+                :particlesInit="particlesInit"
+                :particlesLoaded="particlesLoaded"
+                :options="{
+                    fpsLimit: 60,
+                    interactivity: {
+                        detectsOn: 'canvas',
+                        events: {
+                            onClick: {
+                                enable: true,
+                                mode: 'push'
+                            },
+                            resize: true
+                        },
+                        modes: {
+                            bubble: {
+                                distance: 900,
+                                duration: 2,
+                                opacity: 0.2,
+                                size: 40
+                            },
+                            push: {
+                                quantity: 1
+                            },
+                            repulse: {
+                                distance: 100,
+                                duration: 0.9
+                            }
+                        }
+                    },
+                    particles: {
+                        color: {
+                            value: '#FCEFEF',
+                            opacity: 0.2
+                        },
+                        links: {
+                            color: '#FCEFEF',
+                            distance: 110,
+                            enable: true,
+                            opacity: 0.2,
+                            width: 1
+                        },
+                        move: {
+                            direction: 'none',
+                            enable: true,
+                            outMode: 'bounce',
+                            random: true,
+                            speed: 6,
+                            straight: false
+                        },
+                        number: {
+                            density: {
+                                enable: true,
+                                value_area: 800
+                            },
+                            value: 80
+                        },
+                        opacity: {
+                            value: 0.2
+                        },
+                        shape: {
+                            type: 'circle'
+                        },
+                        size: {
+                            random: true,
+                            value: 5
+                        }
+                    },
+                    detectRetina: true
+                }"
+        />
+  </div>
 </template>
 
 <script>
@@ -11,5 +87,10 @@ export default {
 <style>
   body {
     margin-top: 100px;
+    overflow: hidden;
+  }
+
+  h1 {
+    margin-left: 50px;
   }
 </style>
