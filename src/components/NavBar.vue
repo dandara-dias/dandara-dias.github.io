7665a40ac14b0361205a92da32e661a07ad75eed
dandara-dias <dandaradias.contato@gmail.com>
2022-03-15 12:24:43 -0300

    Mobile First - Navbar - Teste1

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -26,46 +26,87 @@ export default {
 </script>
 
 <style scoped lang="scss">
-  ul {
-  list-style-type: none;
-  margin-top: -92px;
-  padding: 0;
-  overflow: hidden;
-  background-color: rgb(255, 235, 244, 0.9);
-  position: fixed;
-  min-width: 100%;
-  z-index: 1;
-}
+  @media (max-width: 550px) and (max-height: 600px) {
+    ul {
+      list-style-type: none;
+      margin-top: -95px;
+      background-color: rgb(255, 235, 244, 0.9);
+      padding: 0;
+      overflow: hidden;
+      position: fixed;
+      min-width: 100%;
+      z-index: 1;
+    }
 
-li {
-  float: left;
-}
+    li {
+      float: left;
+    }
 
-.social-networks{
-  align-content: center;
-  margin: -16px;
-  margin-left: 5px;
-}
+    li a {
+      display: flex;
+      color: rgb(51, 30, 54);
+      align-items: center;
+      padding: 12px 8px;
+      margin-left: 8px;
+      text-decoration: none;
+      font-family: 'Quicksand', sans-serif;
+    }
 
-li a {
-  display: flex;
-  color: rgb(51, 30, 54);
-  align-items: center;
-  padding: 14px 16px;
-  margin-left: 70px;
-  text-decoration: none;
-  font-family: 'Quicksand', sans-serif;
-}
+    .social-networks {
+      align-content: center;
+      margin: -10px;
+      margin-left: 3px;
+    }
 
-/* Change the link color to #111 (black) on hover */
-li a:hover { //src: w3schools
-  background-color: rgb(187, 139, 181);
-}
+    li a:hover {
+      background-color: rgb(187, 139, 181);
+    }
 
-#navbar-icon{
-  margin-left: 20px;
-}
+    #navbar-icon {
+      margin-left: 6px;
+    }
+  }
 
+  @media (min-width: 551px) {
+    ul {
+      list-style-type: none;
+      margin-top: -92px;
+      padding: 0;
+      overflow: hidden;
+      background-color: rgb(255, 235, 244, 0.9);
+      position: fixed;
+      min-width: 100%;
+      z-index: 1;
+    }
+
+    li {
+      float: left;
+    }
+
+    .social-networks{
+      align-content: center;
+      margin: -16px;
+      margin-left: 5px;
+    }
+
+    li a {
+      display: flex;
+      color: rgb(51, 30, 54);
+      align-items: center;
+      padding: 14px 16px;
+      margin-left: 70px;
+      text-decoration: none;
+      font-family: 'Quicksand', sans-serif;
+    }
+
+    /* Change the link color to #111 (black) on hover */
+    li a:hover { //src: w3schools
+      background-color: rgb(187, 139, 181);
+    }
+
+    #navbar-icon{
+      margin-left: 20px;
+    }
+  }
 @import url('https://fonts.googleapis.com/css2?family=Quicksand&display=swap');
-
 </style>
